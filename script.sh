ac025366e86dab17bbc6b022d13a4eb3823618c9
Mach50 <64970593+MasterMach50@users.noreply.github.com>
2022-01-26 18:56:46 +0530

    script complete

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#preare system
+#prepare system
 echo =========================
 echo Preparing System
 echo =========================
@@ -17,7 +17,25 @@ sudo apt install compton awesome rofi polybar
 echo =========================
 echo Installing Tools
 echo =========================
-sudo apt install neofetch neovim python3-pip
+sudo apt install neofetch neovim python3-pip git wget
+sudo pip3 install pywal colorz
+
+#programs
+echo =========================
+echo Installing Programs
+echo =========================
+mkdir ~/Documents/setup-temp
+wget https://zoom.us/client/latest/zoom_amd64.deb -O ~/Documents/setup-temp/zoom_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/Documents/setup-temp/google-chrome-stable_current_amd64.deb
+dpkg -i ~/Documents/setup-temp/zoom_amd64.deb ~/Documents/setup-temp/google-chrome-stable_current_amd64.deb
+
+#configuration
+echo =========================
+echo Configuring 
+echo =========================
+mkdir ~/Documents/setup-temp/DotFiles
+git clone https://github.com/MasterMach50/DotFiles.git ~/Documents/setup-temp/DotFiles
+cp -r ~/Documents/setup-temp/DotFiles/. ~/.config
 
 #final
 echo =========================
