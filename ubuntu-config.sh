7fadc3c6249e92aa66bd6e6e5b99f53a26b20a18
Mach50 <64970593+MasterMach50@users.noreply.github.com>
2022-03-26 13:25:09 +0530

    temporary changes

--- ubuntu-config.sh
+++ ubuntu-config.sh
@@ -24,32 +24,32 @@ sudo pip3 install pywal colorz
 echo =========================
 echo Installing Programs
 echo =========================
-sudo mkdir /home/$USER/Documents/setup-temp
-sudo wget https://zoom.us/client/latest/zoom_amd64.deb -O /home/$USER/Documents/setup-temp/zoom_amd64.deb
-sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /home/$USER/Documents/setup-temp/google-chrome-stable_current_amd64.deb
-sudo dpkg -i /home/$USER/Documents/setup-temp/zoom_amd64.deb /home/$USER/Documents/setup-temp/google-chrome-stable_current_amd64.deb
+sudo mkdir /home/mathew/Documents/setup-temp
+sudo wget https://zoom.us/client/latest/zoom_amd64.deb -O /home/mathew/Documents/setup-temp/zoom_amd64.deb
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /home/mathew/Documents/setup-temp/google-chrome-stable_current_amd64.deb
+sudo dpkg -i /home/mathew/Documents/setup-temp/zoom_amd64.deb /home/mathew/Documents/setup-temp/google-chrome-stable_current_amd64.deb
 
 
 #configuration
 echo =========================
 echo Configuring 
 echo =========================
-sudo mkdir /home/$USER/Documents/setup-temp/DotFiles
-sudo git clone https://github.com/MasterMach50/DotFiles.git /home/$USER/Documents/setup-temp/DotFiles
-sudo cp -r /home/$USER/Documents/setup-temp/DotFiles/Ubuntu/.config/. /home/$USER/.config
-sudo cp /home/$USER/Documents/setup-temp/DotFiles/Ubuntu/.bashrc /home/$USER
+sudo mkdir /home/mathew/Documents/setup-temp/DotFiles
+sudo git clone https://github.com/MasterMach50/DotFiles.git /home/mathew/Documents/setup-temp/DotFiles
+sudo cp -r /home/mathew/Documents/setup-temp/DotFiles/Ubuntu/.config/. /home/mathew/.config
+sudo cp /home/mathew/Documents/setup-temp/DotFiles/Ubuntu/.bashrc /home/mathew
 
 #final
 echo =========================
 echo Finishing Up
 echo =========================
-sudo rm -rf /home/$USER/Documents/setup-temp/DotFiles
-sudo rmdir /home/$USER/Documents/setup-temp
-sudo chown $USER /home/$USER/.config -R
-sudo chown $USER /home/$USER/.bashrc
-sudo chown $USER /home/$USER/.gitconfig
+sudo rm -rf /home/mathew/Documents/setup-temp/DotFiles
+sudo rmdir /home/mathew/Documents/setup-temp
+sudo chown mathew /home/mathew/.config -R
+sudo chown mathew /home/mathew/.bashrc
+sudo chown mathew /home/mathew/.gitconfig
 
 #change mode of scripts
-sudo chmod +x /home/$USER/.config/polybar/launch.sh
+sudo chmod +x /home/mathew/.config/polybar/launch.sh
 
 sudo apt autoremove
