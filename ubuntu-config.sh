487387d720d4b6d891dac348fe3358fd1d40cf70
Mach50 <64970593+MasterMach50@users.noreply.github.com>
2022-03-26 13:17:32 +0530

    changed ~ to full path

--- ubuntu-config.sh
+++ ubuntu-config.sh
@@ -24,29 +24,32 @@ sudo pip3 install pywal colorz
 echo =========================
 echo Installing Programs
 echo =========================
-sudo mkdir ~/Documents/setup-temp
-sudo wget https://zoom.us/client/latest/zoom_amd64.deb -O ~/Documents/setup-temp/zoom_amd64.deb
-sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/Documents/setup-temp/google-chrome-stable_current_amd64.deb
-sudo dpkg -i ~/Documents/setup-temp/zoom_amd64.deb ~/Documents/setup-temp/google-chrome-stable_current_amd64.deb
+sudo mkdir /home/$USER/Documents/setup-temp
+sudo wget https://zoom.us/client/latest/zoom_amd64.deb -O /home/$USER/Documents/setup-temp/zoom_amd64.deb
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /home/$USER/Documents/setup-temp/google-chrome-stable_current_amd64.deb
+sudo dpkg -i /home/$USER/Documents/setup-temp/zoom_amd64.deb /home/$USER/Documents/setup-temp/google-chrome-stable_current_amd64.deb
 
 
 #configuration
 echo =========================
 echo Configuring 
 echo =========================
-sudo mkdir ~/Documents/setup-temp/DotFiles
-sudo git clone https://github.com/MasterMach50/DotFiles.git ~/Documents/setup-temp/DotFiles
-sudo cp -r ~/Documents/setup-temp/DotFiles/Ubuntu/.config/. ~/.config
-sudo cp ~/Documents/setup-temp/DotFiles/Ubuntu/.bashrc ~
+sudo mkdir /home/$USER/Documents/setup-temp/DotFiles
+sudo git clone https://github.com/MasterMach50/DotFiles.git /home/$USER/Documents/setup-temp/DotFiles
+sudo cp -r /home/$USER/Documents/setup-temp/DotFiles/Ubuntu/.config/. /home/$USER/.config
+sudo cp /home/$USER/Documents/setup-temp/DotFiles/Ubuntu/.bashrc /home/$USER
 
 #final
 echo =========================
 echo Finishing Up
 echo =========================
-sudo rm -rf ~/Documents/setup-temp/DotFiles
-sudo rmdir ~/Documents/setup-temp
-sudo chmod +x ~/.config/polybar/launch.sh
-sudo chown $USER ~/.config -R
-sudo chown $USER ~/.bashrc
-sudo chown $USER ~/.gitconfig
+sudo rm -rf /home/$USER/Documents/setup-temp/DotFiles
+sudo rmdir /home/$USER/Documents/setup-temp
+sudo chown $USER /home/$USER/.config -R
+sudo chown $USER /home/$USER/.bashrc
+sudo chown $USER /home/$USER/.gitconfig
+
+#change mode of scripts
+sudo chmod +x /home/$USER/.config/polybar/launch.sh
+
 sudo apt autoremove
